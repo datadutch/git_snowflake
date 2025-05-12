c3037ea991ea4941d2fab13dde2ed205fa76fa7b
jkooij <john.van.der.kooij@enexis.nl>
2025-05-12 20:52:56 +0200

    from github secrets

--- create_stage.py
+++ create_stage.py
@@ -1,15 +1,21 @@
 import snowflake.connector
 import os
+import json
+
+def get_snowflake_secrets():
+    secrets = json.loads(os.environ['SF_SECRETS'])
+    return secrets['user'], secrets['password'], secrets['account'], secrets['database'], secrets['schema'], secrets['warehouse']
 
 def create_stage():
+    user, password, account, database, schema, warehouse = get_snowflake_secrets()
     with snowflake.connector.connect(
-        user=os.environ['SNOWFLAKE_USER'],
-        password=os.environ['SNOWFLAKE_PASSWORD'],
-        account=os.environ['SNOWFLAKE_ACCOUNT']
+        user=user,
+        password=password,
+        account=account
     ) as conn:
         with conn.cursor() as cur:
-            cur.execute(f'USE DATABASE {os.environ["SNOWFLAKE_DATABASE"]}')
-            cur.execute(f'USE SCHEMA {os.environ["SNOWFLAKE_SCHEMA"]}')
+            cur.execute(f'USE DATABASE {database}')
+            cur.execute(f'USE SCHEMA {schema}')
             cur.execute("CREATE OR REPLACE STAGE st_notebook")
 
 if __name__ == "__main__":
