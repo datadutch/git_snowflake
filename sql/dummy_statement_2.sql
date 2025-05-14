a14deabc8b2343211794186da093b02742455aed
jkooij <john.van.der.kooij@enexis.nl>
2025-05-14 20:49:20 +0200

    2

---
+++ sql/dummy_statement_2.sql
@@ -0,0 +1,9 @@
+-- Dummy SQL statement for testing deployment
+CREATE OR REPLACE TABLE dummy_table (
+    id INT,
+    name STRING,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    PRIMARY KEY (id)
+) COMMENT = 'Dummy table for testing deployment'
+;
